145b269dbadd3ef145ee712274c2493b4801cfbf
Michael Brown <mbrown1413@gmail.com>
2012-10-13 23:55:27 -0400

    refs #1. Fix slug characters in url regex

--- repolab/urls.py
+++ repolab/urls.py
@@ -5,16 +5,16 @@ from repolab import views
 urlpatterns = patterns('',
     url(r'^$', views.Homepage.as_view(), name='homepage_url'),
 
-    url(r'^repo/(?P<slug>[\w\d]+)/$', views.ViewRepo.as_view(), name='repo_url'),
+    url(r'^repo/(?P<slug>[\w\d_-]+)/$', views.ViewRepo.as_view(), name='repo_url'),
 
-    url(r'^repo/(?P<slug>[\w\d]+)/tree/(?P<changeset>[\w\d]+)/$',
+    url(r'^repo/(?P<slug>[\w\d_-]+)/tree/(?P<changeset>[\w\d]+)/$',
         views.ViewChangeset.as_view(), name='repo_changeset_url'),
-    url(r'^repo/(?P<slug>[\w\d]+)/tree/(?P<changeset>[\w\d]+)/(?P<path>.*)/$',
+    url(r'^repo/(?P<slug>[\w\d_-]+)/tree/(?P<changeset>[\w\d]+)/(?P<path>.*)/$',
         views.ViewChangesetPath.as_view(), name='repo_path_url'),
 
-    url(r'^repo/(?P<slug>[\w\d]+)/edit/(?P<changeset>[\w\d]+)/(?P<path>[.*])/$',
+    url(r'^repo/(?P<slug>[\w\d_-]+)/edit/(?P<changeset>[\w\d]+)/(?P<path>[.*])/$',
         views.EditChangesetPath.as_view(), name='repo_edit_url'),
 
-    url(r'^repo/(?P<slug>[\w\d]+)/commit/(?P<changeset>[\w\d]+)/(?P<path>[.*])/$',
+    url(r'^repo/(?P<slug>[\w\d_-]+)/commit/(?P<changeset>[\w\d]+)/(?P<path>[.*])/$',
         views.ViewChangesetCommit.as_view(), name='repo_commit_url'),
 )
